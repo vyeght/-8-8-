4549c2d5ade9bcf1f3c8a74644a69dc2b2915d7a
vyeght <bs1678@ttusd.org>
2024-11-26 11:41:28 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript for interactivity (optional for this basic site)
+document.addEventListener("DOMContentLoaded", () => {
+    // You can add functionality here if needed
+    console.log("Page loaded");
+});
